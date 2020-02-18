f673dbc5ec4a44baf71228e9717cf8cb2e574a96
Harlan Valmer <hvalmer@gmail.com>
2020-02-17 22:49:09 -0300

    Projeto em Java Web com jsf e primefaces

---
+++ primeFacesProj/src/br/com/framework/implementacao/crud/VariavelConexaoUtil.java
@@ -0,0 +1,13 @@
+package br.com.framework.implementacao.crud;
+
+/**
+ * 
+ * @author Harlan
+ * Nome do caminho do JNDI datasource Tomcat
+ *
+ */
+public class VariavelConexaoUtil {
+
+	public static String JAVA_COMP_ENV_JDBC_DATA_SOURCE = "java:/comp/env/jdbc/datasource";
+	
+}
